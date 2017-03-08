6e80f84d487de6e0b52fb7ff806503c168e920f5
christopherdale <christopher.j.dale@baesystems.com>
2017-03-08 14:55:12 +0000

    use auto-redraw

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,10 +1,12 @@
 // Mithril component
 
+var count = 0;
+
 var HelloWorld = {
     view: function() {
         return m("main", [
             m("h1", {class: "title"}, "My first component"),
-            m("button", "a button"),
+            m("button", { onclick: function() {count++} }, count + " clicks"),
         ])
     }
 }
