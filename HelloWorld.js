e4b64f2b233e5c2800e1285f476af62df971dada
christopherdale <christopher.j.dale@baesystems.com>
2017-03-08 15:05:51 +0000

    add xhr

--- HelloWorld.js
+++ HelloWorld.js
@@ -6,7 +6,19 @@ var HelloWorld = {
     view: function() {
         return m("main", [
             m("h1", {class: "title"}, "My first component"),
-            m("button", { onclick: function() {count++} }, count + " clicks"),
+            m("button", { onclick: increment }, count + " clicks"),
         ])
     }
+}
+
+var increment = function() {
+    m.request({
+        method: "PUT",
+        url: "//rem-rest-api.herokuapp.com/api/tutorial/1",
+        data: {count: count + 1},
+        withCredentials: true,
+    })
+    .then(function(data) {
+        count = parseInt(data.count)
+    })
 }
